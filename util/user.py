d9eb0ddd7aed4a1ca8d33b721b7b421109f0d879
kamauvick <kamauvick@outlook.com>
2019-09-02 00:18:44 +0300

    Refactored all the methods to use a json storage instead of the previous csv file.

--- util/user.py
+++ util/user.py
@@ -1,7 +1,8 @@
+import json
 import random
 import string
 
-user_list = 'accounts.json'
+accounts = "accounts.json"
 
 
 def login(username, password):
@@ -9,42 +10,45 @@ def login(username, password):
     pass
 
 
+"""
+Add new users to the user_list array
+"""
+
+
+def add_user(account, name, password):
+    global accounts
+    accounts = list_accounts()
+    accounts.append({'account': account, 'name': name, 'password': password, '': '\n'})
+    _save_user_accounts_(accounts)
+
+
+def list_accounts():
+    with open('accounts.json', 'r') as json_file:
+        return json.load(json_file)
+
+
 def random_pass():
     return ''.join(
         random.choice(string.ascii_letters + string.digits + ".',={}[]-/|\£$%^&*()_+~#@?><") for _ in range(10))
 
 
-def add_user(account, name, password, ):
-    """
-    Add new users to the user_list array
-    """
-    with open(user_list, 'a') as csv_file:
-        csv_file.write(f'{account}, {name}, {password} \n')
+def search_account(name):
+    global accounts
+    accounts = list_accounts()
+    for account in accounts:
+        if account['account'] == name:
+            return account
 
 
-def list_accounts():
-    with open(user_list, 'r') as csv_file:
-        lines = [lines.strip().split(',') for lines in csv_file.readlines()]
-    return [
-        {'Account': line[0], 'Username': line[1], 'Password': line[2]}
-        for line in lines
-    ]
-
-
-def _save_user_accounts(users):
+def _save_user_accounts_(accounts):
     """ Save all user accounts """
-    with open(user_list, 'w') as csv_file:
-        for user in users:
-            csv_file.write(f"{user['Account']}, {user['Username']}, {user['Password']} \n")
+    with open('accounts.json', 'w') as json_file:
+        json.dump(accounts, json_file, sort_keys=True, indent=4)
 
 
 def del_user(name):
     """Delete a user"""
-    user_accounts = list_accounts()
-    for user_account in user_accounts:
-        if user_account['Account'] != name:
-            _save_user_accounts(user_accounts)
-
-# @classmethod
-# def check_user(cls, username):
-#     """Check if a user exists"""
+    global accounts
+    accounts = list_accounts()
+    accounts = [account for account in accounts if account['account'] != name]
+    _save_user_accounts_(accounts)
